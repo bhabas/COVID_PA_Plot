451ab801226942a780a0e13c22a8eab2237b9f92
bhabas <bhabas@psu.edu>
2020-09-17 20:33:55 -0400

    Added State Plotting

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import pkg.Covid_Plots as cp
-
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-
-
-
-
-
-# data_df = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
-
-# fips = cp.fips_lookup("Centre","PA")
-# pop = cp.pop_lookup(fips)
-# print(pop)
-# dates = data_df.columns.values.tolist()[64:]
-
-
-
+import pkg.Covid_Plots as cp
 
 
 
@@ -33,49 +18,11 @@ locations = np.array([
 ["Los Angeles", "CA", "California"],
 ["New York", "NY", "New York"]])
 
-
-# df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csvNew York')
-
-
-
 cp.plot_biwk_sum(locations)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for j in range(0,2):
-#     if j == 0:
-#         locations = ["Erie","Centre"]
-#     elif j==1:
-#         locations = ["Allegheny","Philadelphia"]
-#     else:
-#         pass
-
-#     cp.plot_biwk_sum(county_data,locations,dates,pop_dict)
-        
-
-
-# cp.plot_state_data(state_data, dates)
-
+state = ["Pennsylvania"]
+cp.plot_state_data(state)
 
--- pkg/Covid_Plots.py
+++ pkg/Covid_Plots.py
@@ -16,10 +16,13 @@ def fips_lookup(county,state):
 
 def pop_lookup(county,state):
     df = pd.read_csv('co-est2019-alldata.csv', encoding = "latin")
-    population = df.loc[(df['STNAME'] == state) & (df['CTYNAME'] == county+" County")].values[0,7]
+    population = df.loc[(df['STNAME'] == state) & (df['CTYNAME'] == county+" County")].values[0,18]
     return(population)
 
-
+def state_pop_lookup(state):
+    df = pd.read_csv('co-est2019-alldata.csv', encoding = "latin")
+    population = df.loc[(df['STNAME'] == state) & (df['CTYNAME'] == state)].values[0,18]
+    return(population)
 def plot_biwk_sum(locations):
 
     url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
@@ -82,73 +85,61 @@ def plot_biwk_sum(locations):
     fig.savefig(locations[0,0] + "_biwk_sum.png")
 
 
-def plot_state_data(state_data, dates):
+def plot_state_data(state):
+    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+    df = pd.read_csv(url, delimiter=',')
 
-
-
+    ## Dates Initialization
+    start_index = df.columns.get_loc("3/15/20") # Specified start date of data
+    dates = list(df.columns.values[start_index:])
+    dates = [datetime.strptime(x, '%m/%d/%y') for x in dates]
     today = datetime.today().strftime('%m/%d/%y')
 
-    state_case_delta = state_data[9,1:].astype(int) # New cases per day
-    roll_amount = 7
-    D = pd.Series(state_case_delta)
-    state_delta_mva = D.rolling(roll_amount).mean() # Creates a [7] day rolling average
-    wk_cases_state = sum(state_case_delta[-7:])
 
-    roll_amount = 5
-    pos_rate = state_data[21,1:].astype('str') # Converts to string to remove % then back to float
-    pos_rate = np.char.strip(pos_rate,"%")
-    pos_rate = pos_rate.astype('float64')
-    D = pd.Series(pos_rate)
-    pos_rate_mva = D.rolling(roll_amount).mean() # [5] day rolling average
-    wkavg_pos = int(np.average(pos_rate[-8:]))
-    fig = plt.figure()
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    ax.grid()
 
-    ## ax1 Creation and Plotting
-    ax1 = fig.add_subplot(111)
-    ax1.grid()
+    for i in range(len(state)):
+        population = state_pop_lookup(state[i])
+        pop_factor = population/100e3 # per 100k people
+
+        group = df.loc[(df['Province_State'] == state[i])].values[:,64:]
+        case_delta = np.diff(group, prepend=0)
+        case_delta = np.sum(case_delta,axis=0)
+
+        D = pd.Series(case_delta)
+        d_mvs = D.rolling(7).sum()/pop_factor # new cases in past [14]
 
 
-    l1 = ax1.plot(dates[1:],state_delta_mva,label = "Daily PA Cases")
-    ax1.plot(dates[-7:],state_delta_mva[-7:],linewidth = 5, alpha = 0.6, color = "gray")
+        wk_new_cases = int(np.sum(case_delta[-8:]))
+        total_cases = int(np.sum(case_delta[:]))
 
-    l2 = ax1.plot([],[],color = 'white', label = "Cases in past week: {:,}".format(wk_cases_state))
-    ax1.set_ylim(bottom = 0)
+        plt.plot(dates,d_mvs, label=state[i])
+        plt.plot([],[],label = "Total Cases: " + f"{total_cases:,}", color = "white")
+        plt.plot(dates[-8:],d_mvs[-8:],linewidth = 5, alpha = 0.6, color = "gray")
+        plt.plot([],[],label = "Cases in Past Week: " + f"{wk_new_cases:,}",color = "white")
+        plt.plot([],[],label = "Population: "+ f"{population:,}",color = "white")
 
-
-
-    ax1.set(
+    ax.set(
         ylabel= 'Cases',
-        title = 'PA New COVID-19 Cases per Day ({})'.format(today)
+        xlabel= 'Date',
+        title= 'COVID-19 | Daily Cases per 100k People ({})'.format(today)
         )
 
 
-    ## ax2 Creation and Plotting
-    ax2 = ax1.twinx()
-    l3 = ax2.plot(dates[1:],pos_rate_mva, color = "Orange", label = "Test Positivity Rate")
-    l4 = ax2.plot([],[],color = "white", label = "7-Day Positivity Avg: {:.1f} %".format(wkavg_pos))
-    ax2.plot(dates[-7:],pos_rate_mva[-7:],linewidth = 5, alpha = 0.6, color = "gray")
-    ax2.set_ylabel('Test Positivity Rate (<10% is Good)')
+    plt.ylim(0)
 
+    ax.legend(ncol = len(state), loc='upper center', bbox_to_anchor=(0.5, -0.5))
+    fig.subplots_adjust(bottom = 0.1)
 
-
-    import matplotlib.ticker as mtick
-    ax2.yaxis.set_ticks([0,10,20,40,60,80,100])
-    ax2.yaxis.set_major_formatter(mtick.PercentFormatter())
-    ax2.set_ylim(0, 100)
-
-
-    ax1.xaxis.set_major_locator(mdates.DayLocator(bymonthday=(1,15)))
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%y'))
-    plt.setp(ax1.get_xticklabels(), rotation=30, ha='right')
-
-
-
-    leg = l1 + l2 + l3 + l4
-    labs = [l.get_label() for l in leg]
-    ax1.legend(leg, labs, loc = "upper right")
-
+    ax.xaxis.set_major_locator(mdates.DayLocator(bymonthday=(1,15)))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%y'))
+    plt.setp(ax.get_xticklabels(), rotation=30, ha='right')
 
     plt.tight_layout()
     plt.show()
 
-    fig.savefig('State_Data_fig.png')
+    fig.savefig(state[0] + "daily_cases.png")
+
+
